d9d489687343c0956b6a5ee4903d324f3cf7c60b
sinchang <sinchangwen@gmail.com>
2019-11-25 10:13:12 +0800

    feat: init

--- index.js
+++ index.js
@@ -1,24 +1,54 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const exec = require("@actions/exec");
+const fs = require("fs");
+const initContent = require("./lib/initContent.js");
+const parseComment = require("./lib/parseComment");
 
-
-// most @actions toolkit packages have async methods
 async function run() {
-  try { 
-    const { GITHUB_TOKEN, GITHUB_REPOSITORY } = process.env;
-    const octokit = new github.GitHub(GITHUB_TOKEN);
-    const [owner, repo] = GITHUB_REPOSITORY.split('/');
+  try {
+    const { GITHUB_REPOSITORY } = process.env;
+    core.debug(GITHUB_REPOSITORY);
+    const [projectOwner, projectName] = GITHUB_REPOSITORY.split("/");
 
-    const { data: issuePayload } = await octokit.issues.listEvents({
-      owner,
-      repo,
-    })
+    if (!fs.existsSync(".all-contributorsrc")) {
+      initContent({
+        projectName,
+        projectOwner
+      });
+    }
 
-    core.debug(issuePayload);
-  } 
-  catch (error) {
+    const {
+      comment: {
+        body: commentBody,
+        user: { login: commentUsername },
+        html_url: commentUrl
+      }
+    } = github.context.payload;
+    const { action, who, contributions } = parseComment(commentBody);
+
+    if (!action) {
+      core.setFailed("action only support add");
+    }
+
+    await exec.exec(
+      `npm run contributors:add ${who} ${contributions.join(",")}`
+    );
+    await exec.exec(`npm run contributors:generate`);
+
+    // set env
+    core.exportVariable("branch", `add-${who}`);
+    core.exportVariable("title", `docs: add ${who} as a contributor`);
+    core.exportVariable(
+      "body",
+      `Adds ${who} as a contributor for ${contributions.join(
+        ","
+      )}. \n This was requested by ${commentUsername} in [this comment](${commentUrl})`
+    );
+    core.exportVariable("commitMessage", `docs: add ${who} as a contributor`);
+  } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-run()
+run();
